04218739793c06bdcccf8c2e31c1681cfe4fc08e
YOGESH <YOGESH@DESKTOP-B3HSVTG>
2021-07-19 08:47:32 +0530

    cmd

--- Controllers/StudentController1.cs
+++ Controllers/StudentController1.cs
@@ -16,7 +16,7 @@ namespace WebApplication1.Controllers
         }
         public IActionResult ViewBagSingleValue()
         {
-            ViewBag.Name = "Yogesh";
+            ViewBag.Name = "Yogesh";//using viewBag
             return View();
         }
        
